eaa2e9cbd90f3dc7334e77db14a3b9d02ab94354
ABCALT <1914332326@qq.com>
2026-01-28 21:19:15 +0800

    update backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ import uvicorn
 # from backend.api.video_stream.rtsp_manager import router as rtsp_manager_router
 
 from backend.api.device_management.camera_operation import router as camera_operation_router
-from backend.api.device_management.camera_operation import start_cameras_healthcheck, stop_cameras_healthcheck
+from backend.api.device_management.camera_operation import load_cameras_from_db, save_cameras_to_db, start_cameras_healthcheck, stop_cameras_healthcheck
 
 
 def create_app() -> FastAPI:
@@ -43,8 +43,9 @@ app = create_app()
 
 
 if __name__ == '__main__':
-
+    asyncio.run(load_cameras_from_db())
     asyncio.run(start_cameras_healthcheck())
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000)
     asyncio.run(stop_cameras_healthcheck())
+    asyncio.run(save_cameras_to_db())
 
